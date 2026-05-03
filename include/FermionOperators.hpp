74815df515aaa16b115c38611b85a29d032ab0a4
Rafał Świętek <77swietek77@gmail.com>
2026-05-03 20:21:47 +0200

    Double U(1) symmetry now fast generation

--- include/FermionOperators.hpp
+++ include/FermionOperators.hpp
@@ -62,8 +62,105 @@ namespace operators{
                     );
             };
         }
-    }
-    namespace spin_half{
+        namespace spin_half{
 
+                inline
+                double
+                calculate_sign(u64 base_vec, unsigned int L, int site){
+                    u64 mask = reverseBits( ULLPOW(site)-1, 2*L );
+                    // u64 mask = ULLPOW(site)-1;
+                    // std::cout << "\n" << site << "\t" << boost::dynamic_bitset<>(2*L, base_vec) << "\t" << boost::dynamic_bitset<>(2*L, mask) << std::endl;
+                    return (__builtin_popcountll(base_vec & mask) % 2)? -1 : +1;
+                };
+
+                /// @brief SigmaZ operator on input site
+                /// @tparam _ty template for return type
+                /// @param base_vec Input state to act SigmaZ on
+                /// @param L system size
+                /// @param site site to act operator
+                /// @return pair of return value and resulting state
+                template <typename _ty>
+                inline
+                std::pair<_ty, u64> 
+                density(u64 base_vec, unsigned int L, int site) {
+                    int pos = 2*L - 1 - 2*site;
+                    return std::make_pair(
+                        (_ty)checkBit(base_vec, pos) + (_ty)checkBit(base_vec, pos-1),
+                        base_vec
+                        );
+                };
+
+                /// @brief Anihilate spin-up particle in input site on site site
+                /// @tparam _ty template for return type
+                /// @param base_vec Input state to act operator on
+                /// @param L system size
+                /// @param site site to act operator
+                /// @return pair of return value and resulting state
+                template <typename _ty>
+                inline
+                std::pair<_ty, u64> 
+                create_up(u64 base_vec, unsigned int L, int site) {
+                    double sign = calculate_sign(base_vec, L, 2*site);
+                    int pos = 2*L - 1 - 2*site;
+                    return std::make_pair(
+                        _ty( checkBit(base_vec, pos)? 0.0 : sign ),
+                        flip(base_vec, BinaryPowers[pos], pos)
+                        );
+                };
+
+                /// @brief Anihilate spin-up particle in input site on site site
+                /// @tparam _ty template for return type
+                /// @param base_vec Input state to act operator on
+                /// @param L system size
+                /// @param site site to act operator
+                /// @return pair of return value and resulting state
+                template <typename _ty>
+                inline
+                std::pair<_ty, u64> 
+                anihilate_up(u64 base_vec, unsigned int L, int site) {
+                    double sign = calculate_sign(base_vec, L, 2*site);
+                    int pos = 2*L - 1 - 2*site;
+                    return std::make_pair(
+                        _ty( checkBit(base_vec, pos)? sign : 0.0 ),
+                        flip(base_vec, BinaryPowers[pos], pos)
+                        );
+                };
+
+                /// @brief Create spin-down particle in input site on site site
+                /// @tparam _ty template for return type
+                /// @param base_vec Input state to act operator on
+                /// @param L system size
+                /// @param site site to act operator
+                /// @return pair of return value and resulting state
+                template <typename _ty>
+                inline
+                std::pair<_ty, u64> 
+                create_down(u64 base_vec, unsigned int L, int site) {
+                    double sign = calculate_sign(base_vec, L, 2*site+1);
+                    int pos = 2*L - 2*site - 2;
+                    return std::make_pair(
+                        _ty( checkBit(base_vec, pos)? 0.0 : sign ),
+                        flip(base_vec, BinaryPowers[pos], pos)
+                        );
+                };
+
+                /// @brief Anihilate spin-down particle in input site on site site
+                /// @tparam _ty template for return type
+                /// @param base_vec Input state to act operator on
+                /// @param L system size
+                /// @param site site to act operator
+                /// @return pair of return value and resulting state
+                template <typename _ty>
+                inline
+                std::pair<_ty, u64> 
+                anihilate_down(u64 base_vec, unsigned int L, int site) {
+                    double sign = calculate_sign(base_vec, L, 2*site+1);
+                    int pos = 2*L - 2*site - 2;
+                    return std::make_pair(
+                        _ty( checkBit(base_vec, pos)? sign : 0.0 ),
+                        flip(base_vec, BinaryPowers[pos], pos)
+                        );
+                };
+        }
     }
 }
